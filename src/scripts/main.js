a3439a47a2ae4800ab7e87000ffe042300fdb33f
Mykyta Kalniev <nikitamal4ik@gmail.com>
2025-01-24 21:31:37 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+
+const listOfObj = [];
+
+for (let i = 0; i < list.children.length; i++) {
+  const value = list.children[i].attributes['data-salary'].value.replace(
+    /,/g,
+    '.',
+  );
+
+  listOfObj.push({ salary: Number(value.substring(1)), el: list.children[i] });
+}
+
+const sortedList = listOfObj.sort((a, b) => b.salary - a.salary);
+
+for (const key of sortedList) {
+  list.append(key.el);
+}
