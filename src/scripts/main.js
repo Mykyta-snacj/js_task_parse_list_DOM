0b32e92df9ed914962ac02739f1bb835c8999c16
Mykyta Kalniev <nikitamal4ik@gmail.com>
2025-01-25 13:36:57 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,20 +1,47 @@
 'use strict';
 
-const list = document.querySelector('ul');
+const listElement = document.querySelector('ul');
 
-const listOfObj = [];
+function sortList(list) {
+  const listOfObj = [];
 
-for (let i = 0; i < list.children.length; i++) {
-  const value = list.children[i].attributes['data-salary'].value.replace(
-    /,/g,
-    '.',
-  );
+  for (let i = 0; i < list.children.length; i++) {
+    const value = list.children[i].attributes['data-salary'].value.replace(
+      /,/g,
+      '.',
+    );
 
-  listOfObj.push({ salary: Number(value.substring(1)), el: list.children[i] });
+    listOfObj.push({
+      salary: Number(value.substring(1)),
+      el: list.children[i],
+    });
+  }
+
+  const sortedList = listOfObj.sort((a, b) => b.salary - a.salary);
+
+  for (const key of sortedList) {
+    list.append(key.el);
+  }
+
+  return sortedList;
 }
 
-const sortedList = listOfObj.sort((a, b) => b.salary - a.salary);
+function getEmployees(list) {
+  const employees = [];
 
-for (const key of sortedList) {
-  list.append(key.el);
+  for (let i = 0; i < list.children.length; i++) {
+    const item = list.children[i];
+
+    employees.push({
+      name: item.getAttribute('data-name'),
+      position: item.getAttribute('data-position'),
+      salary: item.getAttribute('data-salary'),
+      age: Number(item.getAttribute('data-age')),
+    });
+  }
+
+  return employees;
 }
+sortList(listElement);
+
+getEmployees(listElement);
